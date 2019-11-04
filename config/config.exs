2bfe3ab036866f2f00640711082f650e3a050721
Alistair A. Israel <aisrael@gmail.com>
2019-11-04 09:45:45 +0800

    Added EventStore

---
+++ config/config.exs
@@ -0,0 +1,11 @@
+use Mix.Config
+
+config :extreme, :event_store,
+  db_type: :node,
+  host: "localhost",
+  port: 1113,
+  username: "admin",
+  password: "changeit",
+  reconnect_delay: 2_000,
+  connection_name: :my_app,
+  max_attempts: :infinity
